734f69b9eff27885abd8a8a7e5d770fc0c8cdb5c
John Dobrota <wickthebest@gmail.com>
2021-01-07 16:33:14 -0800

    Added grey overlay

--- src/components/smallcard/smallcard.js
+++ src/components/smallcard/smallcard.js
@@ -3,17 +3,22 @@ import './smallcard.css'
 
 const baseImageUrl = 'https://image.tmdb.org/t/p/original/'
 
-function smallCard({ type, film }) {
-    // const [hover, setHover] = useState(false);
+function SmallCard({ type, film }) {
+    const [isHover, setHover] = useState(false)
 
     //need to call TMDP API for streaming availability
 
     return (
-        <img
-            className='cardImg'
-            src={`${baseImageUrl}${film.poster_path}`}
-            alt={film.name}
-        />
+        <div className='card'>
+            <img
+                className='cardImg'
+                src={`${baseImageUrl}${film.poster_path}`}
+                alt={film.name}
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+            />
+            <div className='overlay'>Hello</div>
+        </div>
 
         // insert img
         // if hover -> display overlay with following info: film type, title, year, streaming availability
@@ -21,4 +26,4 @@ function smallCard({ type, film }) {
     )
 }
 
-export default smallCard
+export default SmallCard
