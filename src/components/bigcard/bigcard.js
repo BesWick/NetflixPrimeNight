57b24843221eac634fc8294ea9c670c6c173dbf5
BesWick <john.dobrota@gmail.com>
2021-01-11 09:52:41 -0800

    woring on big card css

--- src/components/bigcard/bigcard.js
+++ src/components/bigcard/bigcard.js
@@ -4,6 +4,7 @@ import './bigcard.css'
 
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY
 const baseURL = 'https://api.themoviedb.org/3'
+const baseImageUrl = 'https://image.tmdb.org/t/p/original/'
 
 const latterURL = `?api_key=${API_KEY}&language=en-US&append_to_response=credits`
 
@@ -21,22 +22,31 @@ function BigCard({ filmType, filmID }) {
         }
         fetchData()
     }, [])
-    console.log('title:', filmData.original_title)
+    console.log('filmData:', filmData)
 
     return (
         <div className='bigcard'>
-            <div className='infoRow'>
-                {filmData.release_date?.slice(0, 4)}
-                {filmData?.runtime} min
+            <div className='infoSide'>
+                <div className='infoRow'>
+                    {filmData.release_date?.slice(0, 4)}
+                    {filmData?.runtime} min
+                </div>
+                <div className='titleRow'>{filmData?.original_title}</div>
+                <div className='overviewRow'>
+                    <p>Plot</p>
+                    {filmData?.overview}
+                </div>
+                <div className='lastRow'>
+                    <div className='col1'></div>
+                    <div className='col2'></div>
+                </div>
             </div>
-            <div className='titleRow'>{filmData?.original_title}</div>
-            <div className='overviewRow'>
-                <p>Plot</p>
-                {filmData?.overview}
-            </div>
-            <div className='lastRow'>
-                <div className='col1'></div>
-                <div className='col2'></div>
+            <div className='pictureSide'>
+                <img
+                    className='cardImg'
+                    src={`${baseImageUrl}${filmData.backdrop_path}`}
+                    alt={filmData.name}
+                />
             </div>
         </div>
     )
