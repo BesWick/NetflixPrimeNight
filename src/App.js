2393de001cc7123942d46a718cf9a25996187d04
John Dobrota <wickthebest@gmail.com>
2021-01-24 11:48:34 -0800

    css: changed app layout for sticky searchbar

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import SmallCard from './components/smallcard/smallcard'
 import BigCard from './components/bigcard/bigcard'
+import SearchBar from './components/searchbar/searchbar'
 require('dotenv').config()
 
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY
 const BASE_URL = `https://api.themoviedb.org/3/discover/movie?api_key=`
 
 function App() {
-    const [movies, setMovies] = useState([])
+    const [films, setFilms] = useState([])
     const [bigCardFlag, setBigCardFlag] = useState(false)
     const [bigCardData, setBigCardData] = useState('')
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
         async function fetchData() {
             const response = await axios.get(
                 `${BASE_URL}${API_KEY}&language=en-US&sort_by=revenue.desc&include_adult=false&include_video=false&page=2&with_watch_providers=8%7C9&watch_region=US`,
             )
-            setMovies(response.data.results)
+            setFilms(response.data.results)
+            console.log(response.data.results)
             return response
         }
         fetchData()
     }, [])
-    // console.table(movies)
     const showBigCard = (value, filmType) => {
         console.log(value, filmType)
         setBigCardFlag(!bigCardFlag)
@@ -35,15 +37,23 @@ function App() {
     return (
         <div className='App'>
             {!bigCardFlag && (
-                <div className='container'>
-                    {movies.map((movie) => (
-                        <SmallCard
-                            key={movie.id}
-                            filmType='movie'
-                            film={movie}
-                            logThis={showBigCard}
-                        />
-                    ))}
+                <div className='bigContainer'>
+                    <SearchBar
+                        value={search}
+                        onChange={(e) => {
+                            setSearch(e.target.value)
+                        }}
+                    />
+                    <div className='container'>
+                        {films.map((film) => (
+                            <SmallCard
+                                key={films.id}
+                                filmType='movie'
+                                film={film}
+                                logThis={showBigCard}
+                            />
+                        ))}
+                    </div>
                 </div>
             )}
             {bigCardFlag && (
