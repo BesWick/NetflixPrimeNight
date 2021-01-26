7ace53159001298c61f3488cc1b6c7141454089e
John Dobrota <john.dobrota@gmail.com>
2021-01-25 18:31:57 -0800

    4 search requests working

--- src/App.js
+++ src/App.js
@@ -7,14 +7,9 @@ import SearchBar from './components/searchbar/searchbar'
 import {createDiscoverRequests, createSearchRequests} from './request'
 require('dotenv').config()
 
-const API_KEY = process.env.REACT_APP_TMDB_API_KEY
-const BASE_URL = `https://api.themoviedb.org/3/discover/movie?api_key=`
-const SEARCH_URL = `https://api.themoviedb.org/3/search/multi?api_key=`
-
 function App() {
     const [films, setFilms] = useState([])
     const [cache, setCache] = useState([])
-    const [filmType, setFilmType] = useState([])
     const [bigCardFlag, setBigCardFlag] = useState(false)
     const [bigCardData, setBigCardData] = useState('')
     const [search, setSearch] = useState('')
@@ -39,7 +34,7 @@ function App() {
         fetchData()
     }, [])
     useEffect(() => {
-        if (search == '') {
+        if (search === '') {
             console.log('EMPTY')
             setFilms(cache) //discover
         } else {
@@ -49,19 +44,22 @@ function App() {
     }, [search])
 
     const searchTMDB = async () => {
-        try {
-            const { data } = await axios.get(
-                `${SEARCH_URL}${API_KEY}&language=en-US&query=${search}&region=US`,
-            )
-            // console.log('DATA', data.results)
-            let modifiedArray = data.results.filter(
-                (a) => a.media_type != 'person',
-            )
-            console.log('MODIFIED DATA', modifiedArray)
-            setFilms(modifiedArray)
-        } catch (err) {
-            console.log(err.message)
-        }
+        const reqArray =  createSearchRequests(4, search)
+            const resultArray = []
+            axios.all(reqArray)
+            .then(axios.spread((...responses) => {
+                responses.forEach(response =>{
+                    let data = response.data.results
+                    //  console.log('Success', typeof data, data, response.data)
+                     resultArray.push(...data)
+                    })
+                    console.log('submitted all axios calls', resultArray);
+                    let modifiedArray = resultArray.filter(
+                        (a) => a.media_type !== 'person',
+                    )
+                    setFilms(modifiedArray)
+                    return modifiedArray
+                })).catch(err => console.log(err))
     }
 
     const showBigCard = (value, filmType) => {
